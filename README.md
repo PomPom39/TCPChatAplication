04c8e06c7140bdfc128be3df278d1ade7b09ecf6
PomPom39 <maheshdevadiga001@gmail.com>
2020-05-25 12:45:41 +0530

    Added usage info function

---
+++ README.md
@@ -0,0 +1,1 @@
+# A TCP based Server-Client Chat Program using Socket Programming in C
--- src/mainApp.c
+++ src/mainApp.c
@@ -5,12 +5,27 @@
 #include <stdlib.h>
 #include "../libs/netconf.h"
 #include <pthread.h>
-
+#include <string.h>
 
 
 netconf_t servObj;
 pthread_t tid[3];
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+
+void usage() {
+
+	printf("\n ");
+	printf("\n Usage: mainApp -m [MODE] -i [IP_ADDRESS] -p [PORT]");
+	printf("\n [MODE]: 'server' or 'client' ");
+	printf("\n [IP_ADDRESS] :if client then IP to connect to");
+	printf("\n [PORT]: port no to connect or to host" );
+
+	printf("\n");
+	exit(1);
+}
 
 
 void *cliThread(void *args) {
@@ -26,14 +41,86 @@ void *cliThread(void *args) {
 
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv) {
 
+
+	char c;
+	int err;
+	char *mode, *ip;
+	int mflag=0, pflag=0, iflag=0, errflag=0;
+	int modeflag = 0;
+	char *port;
 	int cliSock[3];
-	printf("\n Hello World");
 	char clientname[100];
 
+	if (argc < 3) {
+		usage();
+		exit(1);
+	}
 
-	servObj.port = 12345;
+	while ((c = getopt(argc, argv, ":m:p:i:")) != -1) {
+		switch(c) {
+			case 'm':
+				mflag = 1;
+				if (strcmp ("server", optarg) == 0) {
+					iflag = 1;
+					modeflag = 1;
+				}
+				else if (strcmp ("client", optarg) == 0) {
+					iflag = 0;
+				}
+				else {
+					errflag = 1;
+				}
+				mode = optarg;
+				break;
+			case 'p':
+				pflag = 1;
+				port = optarg;
+				break;
+			case 'i':
+				if (!iflag) {
+					iflag = 1;
+					ip = optarg;
+				}
+				break;
+			case '?':
+				errflag = 1;
+				printf("\n Unknown argument");
+				usage();
+				break;
+		}
+
+	}
+		
+	if (mflag == 0 || pflag == 0) {
+		printf("\n Missing arguments");
+		usage();
+	}
+
+	if (iflag == 0) {
+		printf("\n Missing IP");
+		usage();
+	}
+		
+	if (errflag == 1) {
+		printf("\n Wrong arguments, please check");
+		usage();
+	}
+
+
+	printf("\n m=%s \n p=%s \n i =%s \n err=%d", mode, port, ip, err);
+
+
+
+	if (modeflag == 1) {
+	/* Server Code Implemetation */
+	
+
+	servObj.port = atoi(port);
+
+
+
 	
 	printf("\n Initializing TCP Network Parameters");
 	netInit(&servObj, 1); 		//Initialize the server
@@ -58,14 +145,23 @@ int main(int argc, char* argv[]) {
 		}
 		printf("\n Thread %d created", i);
 		fflush(stdout);
-}
+	}
 
 
-fflush(stdout);
+	fflush(stdout);
 
 
 	while (1) {
 		sleep(1);
 	}
+
+}
+
+
+else {
+	/* Client Code (not yet implemeted)*/
+	printf("\n Not yet implemented");
+
+}
 
 }
